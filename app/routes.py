f6e4ce39e27f3b15fd521b690040e5d3af846f5b
Azamat <azahanbabaev@gmail.com>
2020-11-15 17:08:20 -0500

    fixed sign up form

--- app/routes.py
+++ app/routes.py
@@ -104,6 +104,17 @@ def login():
     #         return redirect(url_for('home'))
     return render_template(('login.html'))
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #         error = 'Invalid credentials. Please try again'
+    #     else:
+    #         session['logged_in'] = True
+    #         return redirect(url_for('home'))
+    return render_template(('signup.html'))
+
 
 @app.route('/welcome')
 def welcome():
