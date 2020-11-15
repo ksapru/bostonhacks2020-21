9f04f1e88e60bf82c42fc1f8989b70ffb430e4c9
Smarangk13 <smarangk@gmail.com>
2020-11-14 21:05:24 -0500

    Deleted django stuff

--- HiveMind/imageClassify/FileClassify.py
+++
@@ -1,6 +0,0 @@
-class Files:
-    def __init__(self):
-        pass
-
-    def f1(self):
-        return "124"
--- HiveMind/imageClassify/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageclassifyConfig(AppConfig):
-    name = 'imageClassify'
